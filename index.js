00ed55c3a01c5ce048bf2d31eaea2e8199dff951
abazsh3 <49361636+abazsh3@users.noreply.github.com>
2020-09-11 23:06:07 +0430

    fixed start command and added @ to usernames

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const eol = require('os').EOL;
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
+bot.start(ctx => {
+    ctx.reply("با این ربات میتونی پیام دلخواهت رو به ادمین کانال roast put ارسال کنی!" +
+        "از این به بعد هر پیامی که بفرستی مستقیم به ادمین ارسال میشه! " , )
+        .then(()=>ctx.telegram.sendMessage('-1001387393875','new user :'+eol+ctx.message.from.id+eol+ctx.message.from.first_name+eol+'@'+ctx.message.from.username))
+        .catch(e=>ctx.telegram.sendMessage('-1001387393875','error:' +e))
+})
 bot.on('text', (ctx) => {
-    if (ctx.message.text==='/start'){
 
-        ctx.reply("با این ربات میتونی پیام دلخواهت رو به ادمین کانال roast put ارسال کنی!" +
-            "از این به بعد هر پیامی که بفرستی مستقیم به ادمین ارسال میشه! " , )
-            .then(()=>ctx.telegram.sendMessage('-1001387393875','new user :'+eol+ctx.message.from.id+eol+ctx.message.from.first_name+eol+ctx.message.from.username))
-            .catch(e=>ctx.telegram.sendMessage('-1001387393875','error:' +e))
-    }
-    else {
+
         ctx.telegram.forwardMessage(400838874,ctx.message.from.id,ctx.message.message_id)
             .then(()=>{
                 ctx.telegram.forwardMessage('-1001387393875',ctx.message.from.id,ctx.message.message_id).catch(e=>console.log(e))
@@ -21,8 +20,8 @@ bot.on('text', (ctx) => {
                 ctx.telegram.forwardMessage(99779769,ctx.message.from.id,ctx.message.message_id).catch(e=>console.log(e))
             })
             .catch(e=>ctx.telegram.sendMessage('-1001387393875','error:' +e))
-    }
+
 
 })
-bot.launch()
+bot.launch().then(()=>console.log('successfully launched')).catch(e=>console.log('error launching : ' +e))
 
